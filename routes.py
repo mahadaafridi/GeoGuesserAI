c7630414dc0792482001b7f169340c6ba4a05773
Mahad Afridi <mahadakbarafridi@gmail.com>
2025-02-18 13:19:54 -0800

    basic flask template

---
+++ routes.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# two decorators, same function
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template('index.html', the_title='Home Page')
+
+@app.route('/test.html')
+def test():
+    return render_template('test.html', the_title='Test')
+
+if __name__ == '__main__':
+    app.run(debug=True)
